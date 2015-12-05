b15848555d652c4464b3abd6a64f0fb28fced6aa
Federico Capoano <nemesis@ninux.org>
2015-12-05 12:11:29 +0100

    [RFC] Improved xml2epub.py (manage failure)

--- rfc/xml2epub.py
+++ rfc/xml2epub.py
@@ -8,6 +8,10 @@ params = {
 }
 
 response = requests.post(form_url, params)
-f = open('./netjson.epub', 'w')
-f.write(response.content)
-f.close()
+if response.status_code == 200:
+    f = open('./netjson.epub', 'w')
+    f.write(response.content)
+    f.close()
+    print('Generated RFC in ePub format')
+else:
+    print("Error: response code was: {0}".format(response.status_code))
